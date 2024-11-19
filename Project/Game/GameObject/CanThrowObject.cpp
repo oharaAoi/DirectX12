cda68ef36bb5c715be6fe717d3ae48a872d8d9fa
oharaAoi <k023g0108@g.neec.ac.jp>
2024-11-20 02:30:29 +0900

    エラーの修正

--- Project/Game/GameObject/CanThrowObject.cpp
+++ Project/Game/GameObject/CanThrowObject.cpp
@@ -1,6 +1,6 @@
 #include "CanThrowObject.h"
 
-void CanThrowObject::SetThrow(const Vector3& velocity) {
+void CanThrowObject::ReadyToThrow(const Vector3& velocity) {
 	velocity_ = velocity;
 	isThrowed_ = true;
 }
