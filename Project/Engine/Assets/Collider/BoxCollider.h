e02f78144e320f68df7515b5153f4a4d6b61d3df
oharaAoi <k023g0108@g.neec.ac.jp>
2024-12-31 18:44:20 +0900

    colliderの修正

--- Project/Engine/Assets/Collider/BoxCollider.h
+++ Project/Engine/Assets/Collider/BoxCollider.h
@@ -13,12 +13,6 @@ public:
 	void Update(const SRT& srt) override;
 	void Draw() const override;
 
-public:
-
-	void SetSize(const Vector3& size) { size_ = size; }
-	
 private:
-
-	Vector3 size_;
 
 };
